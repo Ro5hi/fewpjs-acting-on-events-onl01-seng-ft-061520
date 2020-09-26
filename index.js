bd67d3a97650dcb6c77d5b6273403a0daa04f4a4
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-26 00:46:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,33 @@
 console.log(dodger.style.left); 
 console.log(dodger.style.bottom);
 dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown",
+function (e) {
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left - 1}px`;
+  }
+});
+
+function moveDodgerLeft() {
+  let leftNumbers = 
+  dodger.style.left.replace("px","");
+  let left = parseInt(leftNumbers, 10);
+  
+  if (left > 0) {
+    dodger.style.left =  `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+  }
+}
